2a83120fbb2ab73749d52df0331b165b71aa1f9c
AlexWatabe9 <alexwatabe@protonmail.com>
2023-02-13 14:07:58 -0700

    this is my first commit

---
+++ script.js
@@ -0,0 +1,89 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var uppercaseCharacter = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowercaseCharacter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numberCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var characterCriteria = [""];
+// Write password to the #password input
+
+function generatePassword() {
+  var passwordLength = parseInt(prompt(
+    "Please enter number of characters from 8 to 128"
+  ));
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter number between 8 and 128 ");
+    return
+  }
+  
+  var uppercaseCharacter = confirm("Do you want Capital Letters in your password?");
+  
+
+  return "hello";
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
